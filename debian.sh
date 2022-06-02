0f8697913bdbd31c43453f18ce9096542c53df2a
Jason Javandel <53279269+CherryYeti@users.noreply.github.com>
2022-06-01 17:12:24 -0700

    Add files via upload

---
+++ debian.sh
@@ -0,0 +1,23 @@
+
+echo "################################################################"
+echo "#### Osu on debian linux install script, made by CherryYeti ####"
+echo "################################################################"
+sudo apt update && sudo apt upgrade && sudo apt install git curl build-essential zstd
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
+sudo apt update
+sudo apt install --install-recommends winehq-staging
+sudo apt install winetricks
+git clone https://gitlab.com/osu-wine/osu-wine
+cd osu-wine
+sudo ./install.sh
+osu-wine
+wget --no-check-certificate 'https://docs.google.com/uc?>wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xgJIe18ccBx6yjPcmBxDbTnS1XxwrAcc' -O wine-osu-7.0-x86_64.pkg.tar.zst
+tar -xf wine-osu-7.0-x86_64.pkg.tar.zst
+sudo cp -r ./opt/wine-osu/ /opt
+sudo nano /etc/osu-wine.conf
+echo "################################################################"
+echo "##### The install has been completed. Enjoy your suffering #####"
+echo "################################################################"
